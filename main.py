49f50a996ca57646a07deca76c84509b72f5bfff
Yan <83599393+tribal666@users.noreply.github.com>
2026-03-04 12:20:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import requests
 
-BOT_TOKEN = os.environ["8669894437:AAGZqV3WGybOafbE48tPVGxqVsn3TkKNjAg"]
-CHAT_ID = os.environ["6526554977"]
+BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
+CHAT_ID = os.environ["TG_CHAT_ID"]
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+data = {"chat_id": CHAT_ID, "text": "✅ Test GitHub → Telegram OK"}
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": "✅ Test GitHub → Telegram OK"
-}
-
-requests.post(url, json=data)
+r = requests.post(url, json=data, timeout=20)
+r.raise_for_status()
+print("Sent OK")
