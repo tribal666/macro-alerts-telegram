82c793c35be319bf157cf5b650b4d453017cf647
Yan <83599393+tribal666@users.noreply.github.com>
2026-03-06 12:16:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,18 +21,7 @@ STATE_FILE = Path("state.json")
 
 # Paramètres
 ALLOWED_CURRENCIES = {"USD", "EUR", "GBP"}
-ALLOWED_IMPACT = {"Medium", "High"}
-
-    # toujours autoriser HIGH
-    if impact == "High":
-        return True
-
-    # MEDIUM seulement pour USD
-    if impact == "Medium" and currency == "USD":
-        return True
-
-    return False
-    
+ALLOWED_IMPACT = {"Medium", "High"}  
 
 def is_allowed_event(ev):
     impact = ev["impact"]
